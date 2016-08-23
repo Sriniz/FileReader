80a69c03df282b5fa346babecbc7b498c2eff7b3
Srinizkumar Konakanchi <srinizkumar.konakanchi@wbconsultant.com>
2016-08-23 16:13:20 -0700

    Changed Connection to use API 37.0

--- src/com/xlsxReadWrite/Main.java
+++ src/com/xlsxReadWrite/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
 		// Salesforce.com credentials
 		String userName = "upload@test.com";
-		String password = "Indian06#";
+		String password = "Password#123";
 		String endPnt = "https://test.salesforce.com/services/Soap/u/37.0";
 
 		BulkLoader example = new BulkLoader();
